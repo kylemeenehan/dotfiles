e5cda40468cefdf539c66a8735787e81fc8889ce
Kyle Meenehan <kylemeenehan@gmail.com>
2020-12-15 09:07:33 +0000

    updated setup script

--- setup.sh
+++ setup.sh
@@ -4,21 +4,21 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo $DIR
 
-declare -A links=(
-  ["vim/ftplugin"]="$HOME/.vim/ftplugin"
-  [""]="$HOME/."
-  [""]="$HOME/."
-  [""]="$HOME/."
-  [""]="$HOME/."
-  [""]="$HOME/."
-  [""]="$HOME/."
+declare homelinks=( "eslintrc.json"
+  "agignore"
+  "alacritty.yml"
+  "gitconfig"
+  "ideavimrc"
+  "tmux.conf"
+  "vimrc"
+  "zshrc"
 )
 
-for link in "${!links[@]}"; do
-  # echo "linking $DIR/$link  ${links[$link]}";
-  echo $DIR/$link ${links[$link]}
-  unlink ${links[$link]}
-  ln -s $DIR/$link ${links[$link]}
+for link in "${homelinks[@]}"; do
+  echo linking $DIR/$link to $HOME/.$link
+  echo linking $DIR/$link to $HOME/.$link
+  ln -sf "$DIR/$link" "$HOME/.$link"
+  # unlink $HOME/.$link
 done
 
-# ln -s $DIR/vim/ftplugin $HOME/.vim/ftplugin
+ln -sf "$DIR/vim/ftplugin" "$HOME/.vim/ftplugin"
